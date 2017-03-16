d129c3a7b9da7e740ed50f2559e5725f9562233b
justingerolami <justin.gerolami@queensu.ca>
2017-03-16 17:45:55 -0400

    Added slider, functionality, volume, working logic
    Added:
    1. A double slider that allows the user to select the maximum and
    minimum threshold value.
    
    2. Input and output volume nodes that allow the user to select the
    input volume and output volume
    
    3. Complete functionality of the slider, inputs, apply button.
    
    4. Added functions to logic that work with the apply button, but also
    work with the automatic testing.
    
    5. Added run() to logic that runs the main code so far

--- SpineSegmentation/SpineSegmentation.py
+++ SpineSegmentation/SpineSegmentation.py
@@ -51,42 +51,76 @@ class SpineSegmentationWidget(ScriptedLoadableModuleWidget):
     self.layout.addWidget(parametersCollapsibleButton)
     parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
 
-    #Create a slider that allows user to set min threshold
-    self.minThresholdSlider = ctk.ctkSliderWidget()
-    self.minThresholdSlider.singleStep = 1
-    self.minThresholdSlider.minimum = 0
-    self.minThresholdSlider.maximum = 255
-    self.minThresholdSlider.value = 0
-    self.minThresholdSlider.setToolTip(
-      "Voxels with intensities lower than this threshold will be 0")
-    parametersFormLayout.addRow("minThreshold", self.minThresholdSlider)
+    #Create the input selector for input volume
+    self.inputSelector = slicer.qMRMLNodeComboBox()
+    self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.inputSelector.selectNodeUponCreation = False
+    self.inputSelector.addEnabled = False
+    self.inputSelector.removeEnabled = False
+    self.inputSelector.noneEnabled = True
+    self.inputSelector.showHidden = False
+    self.inputSelector.showChildNodeTypes = False
+    self.inputSelector.setMRMLScene(slicer.mrmlScene)
+    self.inputSelector.setToolTip("Select the input.")
+    parametersFormLayout.addRow("Input Image Volume: ", self.inputSelector)
 
-    #Create a slider that allows user to set max threshold
-    self.maxThresholdSlider = ctk.ctkSliderWidget()
-    self.maxThresholdSlider.singleStep = 1
-    self.maxThresholdSlider.minimum = 0
-    self.maxThresholdSlider.maximum = 255
-    self.maxThresholdSlider.value = 0
-    self.maxThresholdSlider.setToolTip(
-      "Voxels with intensities higher than this threshold will be 1")
-    parametersFormLayout.addRow("maxThreshold", self.maxThresholdSlider)
+    #Create the output selector for output volume
+    self.outputSelector = slicer.qMRMLNodeComboBox()
+    self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.outputSelector.selectNodeUponCreation = True
+    self.outputSelector.addEnabled = True
+    self.outputSelector.removeEnabled = True
+    self.outputSelector.noneEnabled = True
+    self.outputSelector.showHidden = False
+    self.outputSelector.showChildNodeTypes = False
+    self.outputSelector.setMRMLScene(slicer.mrmlScene)
+    self.outputSelector.setToolTip("Select the output.")
+    parametersFormLayout.addRow("Output Image Volume: ", self.outputSelector)
 
-    #TODO
-    # Add apply button
+    #Create a slider that allows user to set threshold
+    self.ThresholdSlider = ctk.ctkRangeWidget()
+    self.ThresholdSlider.singleStep = 1
+    self.ThresholdSlider.minimum = -100
+    self.ThresholdSlider.maximum = 100
+    self.ThresholdSlider.setToolTip(
+      "Set the minimum and maximum threshold for use with BinaryThreshold")
+    parametersFormLayout.addRow("Threshold", self.ThresholdSlider)
 
+    #Create the apply button
+    self.applyButton = qt.QPushButton("Apply")
+    self.applyButton.toolTip = "Run the algorithm."
+    self.applyButton.enabled = False
+    parametersFormLayout.addRow(self.applyButton)
 
-    #TODO
-    # Add connections
+    # connections
+    self.applyButton.connect('clicked(bool)', self.onApplyButton)
+    self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+
+    # Add vertical spacer
+    self.layout.addStretch(1)
+
+    # Refresh Apply button state
+    self.onSelect()
 
 
   def cleanup(self):
     pass
 
+
   def onSelect(self):
-    pass
+    self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
 
   def onApplyButton(self):
-    pass
+    #When apply button is hit, get the threshold value
+    minValue = self.ThresholdSlider.minimumValue
+    maxValue = self.ThresholdSlider.maximumValue
+    #print it to console
+    print("Threshold has been set to: Min: " + str(minValue) + ", Max: " + str(maxValue))
+    #Call the logic class with the selectors and threshold
+    logic = SpineSegmentationLogic()
+    logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode(), minValue, maxValue)
+
 
 #
 # SpineSegmentationLogic
@@ -101,6 +135,67 @@ class SpineSegmentationLogic(ScriptedLoadableModuleLogic):
   Uses ScriptedLoadableModuleLogic base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
+
+  def addFilterToImage(self, inputImage, outputName):
+    '''
+    :param image, outputName: The image that requires a filter and the output name
+    :return: None, adds the filter and adds to slicer
+    '''
+
+    #Pull it from slicer
+    image = sitkUtils.PullFromSlicer(inputImage)
+    #Use the gaussian image filter for smoothing
+    #https://itk.org/SimpleITKDoxygen/html/classitk_1_1simple_1_1SmoothingRecursiveGaussianImageFilter.html#details
+    #Will experiment with others later
+    imageFilter = SimpleITK.SmoothingRecursiveGaussianImageFilter()
+    #Execute the filter on the image
+    smoothedImage = imageFilter.Execute(image)
+    #Add it to slicer, overwrite the current node
+    #True means overwrite the outputName instead of creating a new one
+    sitkUtils.PushToSlicer(smoothedImage, outputName, 0, True)
+
+
+  def thresholdImage(self, inputImage, outputName, minValue=0, maxValue=100):
+    '''
+    :param image, outputName, threshold values: image that requires threshold, output name, and threshold values
+    Executes a threshold filter on the image and pushes it back to slicer.
+    '''
+
+    #Pull it from slicer
+    image = sitkUtils.PullFromSlicer(inputImage)
+    #Set the filter
+    thresholdFilter = SimpleITK.BinaryThresholdImageFilter()
+    #Set the value for something inside the threshold to be 1
+    thresholdFilter.SetInsideValue(1)
+    #Set the value for something outside the threshold to be 0
+    thresholdFilter.SetOutsideValue(0)
+    #Set the max and min threshold values
+    thresholdFilter.SetLowerThreshold(minValue)
+    thresholdFilter.SetUpperThreshold(maxValue)
+    #execute the filter on the image
+    thresholdedImage = thresholdFilter.Execute(image)
+    #push it to slicer, overwrite current output node
+    sitkUtils.PushToSlicer(thresholdedImage, outputName, 0, True)
+
+
+  def run(self, inputVolume, outputVolume, minValue, maxValue):
+    '''
+    :param inputVolume: the input image volume
+    :param outputVolume: the output image volume
+    :param minValue: the min threshold selected by user
+    :param maxValue: the max threshold select by user
+
+    Gets the inputVolume name, pulls the image from slicer, adds the filter and threshold
+    based on the user defined variables
+    '''
+    #Get the input image name
+    inputImage = inputVolume.GetName()
+
+    #Add the filter to the image
+    self.addFilterToImage(inputImage, outputVolume.GetName())
+
+    #Threshold the image with user-set threshold values
+    self.thresholdImage(inputImage, outputVolume.GetName(), minValue, maxValue)
 
 
 #
@@ -138,6 +233,21 @@ class SpineSegmentationTest(ScriptedLoadableModuleTest):
     #There is no '/' so the imagePath should just be the file name.
     return imagePath
 
+
+  def loadImage(self, imagePath):
+    """
+    :param imagePah: takes the image path as a parameter.
+    :return: the image pulled from slicer
+    """
+    #Load the image from a hard-coded path
+    #Future updates can allow the user to change this
+    slicer.util.loadVolume(imagePath)
+    #Get the file name and extension
+    imageName = self.getImageFileName(imagePath)
+    #Now we want just the filename
+    imageName = self.getImageName(imageName)
+    return imageName
+
   def getImageName(self, imageName):
     '''
     :param imageName: The name of the image with extension and maybe starting '/'
@@ -156,76 +266,20 @@ class SpineSegmentationTest(ScriptedLoadableModuleTest):
       #returns the image name if no '.' was found
       return imageName
 
-
-  def loadImage(self, imagePath):
-    """
-    :param imagePah: takes the image path as a parameter.
-    :return: the image pulled from slicer
-    """
-    #Load the image from a hard-coded path
-    #Future updates can allow the user to change this
-    slicer.util.loadVolume(imagePath)
-    #Get the file name and extension
-    imageName = self.getImageFileName(imagePath)
-    #Now we want just the filename
-    imageName = self.getImageName(imageName)
-    #We want to get the image name, which is after the last '/'
-    #Now that we have the image loaded, pull the name from slicer.
-    loadedImage = sitkUtils.PullFromSlicer(imageName)
-    return loadedImage
-
-
-  def addFilterToImage(self, image):
-    '''
-    :param image: The image that requires a filter
-    :return: None, adds the filter and adds to slicer
-    '''
-    #Use the gaussian image filter for smoothing
-    #https://itk.org/SimpleITKDoxygen/html/classitk_1_1simple_1_1SmoothingRecursiveGaussianImageFilter.html#details
-
-    #Will experiment with others later
-    imageFilter = SimpleITK.SmoothingRecursiveGaussianImageFilter()
-    #Execute the filter on the image
-    smoothedImage = imageFilter.Execute(image)
-    #Add it to slicer
-    sitkUtils.PushToSlicer(smoothedImage, 'Smoothed Image')
-
-  def thresholdImage(self):
-    '''
-    Executes a threshold filter on the image and pushes it back to slicer.
-    '''
-    #Set the filter
-    thresholdFilter = SimpleITK.BinaryThresholdImageFilter()
-
-    #Get the node of the current image
-    #imageNode = slicer.util.getNode('Smoothed Image')
-    #imageToThreshold = imageNode.GetImageData()
-
-    #Pull the image from slicer
-    imageToThreshold = sitkUtils.PullFromSlicer('Smoothed Image')
-    #Set the value for something inside the threshold to be 1
-    thresholdFilter.SetInsideValue(1)
-    #Set the value for something outside the threshold to be 0
-    thresholdFilter.SetOutsideValue(0)
-
-    #execute the filter on the image
-    thresholdedImage = thresholdFilter.Execute(imageToThreshold)
-    #push it to slicer
-    sitkUtils.PushToSlicer(thresholdedImage, 'Smoothed and Thresholded Image')
-
   def test_SpineSegmentation1(self):
     """
     test_SpineSegmentation1 is a test that will load an image from a file path, display the image, and run a filter
     on the image to smooth it out.
     """
+    #Setup logic
+    logic = SpineSegmentationLogic()
 
     self.delayDisplay("Running test to load and smooth image.")
     #Load the image
     testImage = self.loadImage("/Users/Justin/GitHub/CISC472_SpineSegmentation/SpineData/007.CTDC.nrrd")
     #Add the filter
-    self.addFilterToImage(testImage)
+    logic.addFilterToImage(testImage, "Smoothed Image")
     self.delayDisplay("Thresholding image.")
-    self.thresholdImage()
+    #Add threshold
+    logic.thresholdImage("Smoothed Image", "Smoothed and Threshold Image")
     self.delayDisplay("Testing complete.")
-
-    
